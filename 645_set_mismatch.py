ec8272834249fddf2c508fe6fa08ac3aa2637c14
Navya <navyaninja@gmail.com>
2026-07-09 20:22:18 +0530

    Solve LeetCode 645: Set Mismatch

---
+++ 645_set_mismatch.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        freq = [0]*(len(nums)+1)
+        for num in nums:
+            freq[num] += 1
+        duplicate = 0
+        missing = 0
+
+        for i in range(1,len(nums)+1):
+            if freq[i] == 2:
+                duplicate = i
+            if freq[i] == 0:
+                missing = i
+        return(duplicate,missing)
